813fdbfd9542597bf109d59329c54dbd41f91e94
msoo6465 <msoo6465@gmail.com>
2021-04-26 13:55:28 +0900

    Add title & description sql

--- nodejs/main.js
+++ nodejs/main.js
@@ -21,17 +21,8 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname == '/'){
       if(queryData.id === undefined){
-        // fs.readdir('./data',function(error, filelist){
-        //   var title = 'Welcome';
-        //   var description = 'Hello, Node.js'
-        //   var list = template.list(filelist);
-        //   var html = template.HTML(title, list, `<h2>${title}</h2><p>${description}`,
-        //   `<a href="/create">create</a>`)
-        //   response.writeHead(200);
-        //   response.end(html);
-        // });
+
         db.query(`SELECT * FROM topic`,function(error,topics){
-          console.log(topics);
           var title = 'Welcome';
           var description = 'Hello, Node.js'
           var list = template.list(topics);
@@ -43,6 +34,29 @@ var app = http.createServer(function(request,response){
         })
 
       }else {
+        db.query(`SELECT * FROM topic`,function(error,topics){
+          if(error){
+            throw error;
+          }
+          db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id],function(error2,topic){
+            if(error2){
+              throw error2
+            }
+            var title = topic[0].title;
+            var description = topic[0].description;
+            var list = template.list(topics);
+            var html = template.HTML(title, list,`<h2>${title}</h2><p>${description}`,
+              `<a href="/create">create </a> <a href="/update?id=${queryData.id}">update</a>
+          <form action="delete_process" method="post" onsubmit="really?">
+            <input type="hidden" name="id" value=${queryData.id}>
+            <input type="submit" value="delete">
+          </form>`)
+            console.log(topic[0].title);
+            response.writeHead(200);
+            response.end(html);
+          })
+        })
+        /*
         fs.readdir('./data',function(error, filelist){
           var filteredPath = path.parse(queryData.id).base
           fs.readFile(`data/${filteredPath}`,'utf8',function(err,description){
@@ -57,10 +71,10 @@ var app = http.createServer(function(request,response){
               <input type="submit" value="delete">
             </form>`
           );
-            response.writeHead(200);
-            response.end(html);
+
         });
       });
+      */
       }
     }else if(pathname==='/create'){
       if(queryData.id === undefined){
